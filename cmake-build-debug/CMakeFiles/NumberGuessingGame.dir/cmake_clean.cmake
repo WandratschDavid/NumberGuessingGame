9c13b1cbfd6dbbc6ce927cb05fe781529585e176
David Wandratsch <david.wandratsch@htl-wels.at>
2021-03-25 09:37:23 +0100

    upload from the project to github

---
+++ cmake-build-debug/CMakeFiles/NumberGuessingGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumberGuessingGame.dir/main.c.obj"
+  "NumberGuessingGame.exe"
+  "NumberGuessingGame.exe.manifest"
+  "NumberGuessingGame.pdb"
+  "libNumberGuessingGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NumberGuessingGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
